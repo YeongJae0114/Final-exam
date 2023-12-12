bb8bcb69006a83a8e3de2afd948c509f8b86464a
YeongJae0114 <yjl8628@naver.com>
2023-12-12 20:53:57 +0900

    :bug: 오류 수정과 주석 추가
    코드가 실행되는지 다시 확인해봄

--- Final_exam.py
+++ Final_exam.py
@@ -16,7 +16,7 @@ import time
 #
 # 제한사항
 # 1 ≤ my_string 의 길이 ≤ 100
-# my_string 은 영소문자로만 이루어져 있습니다.
+# my_string 은 영소문자로만 이루어져 있습니다. 
 # 1 ≤ target 의 길이 ≤ 100
 # target 은 영소문자로만 이루어져 있습니다.
 
@@ -31,8 +31,8 @@ def solution(my_string, target):
     return answer
 
 
-# print(solution('I love python', 'python'))
-# print(solution('I love python', 'Java'))
+#print(solution('I love python', 'python'))
+#print(solution('I love python', 'Java'))
 
 
 # Q.2 10점
@@ -52,34 +52,14 @@ def solution(my_string, target):
 
 def solution(letter):
     ## 모스부호 딕셔너리
-    morse = {
-        ".-": "a",
-        "-...": "b",
-        "-.-.": "c",
-        "-..": "d",
-        ".": "e",
-        "..-.": "f",
-        "--.": "g",
-        "....": "h",
-        "..": "i",
-        ".---": "j",
-        "-.-": "k",
-        ".-..": "l",
-        "--": "m",
-        "-.": "n",
-        "---": "o",
-        ".--.": "p",
-        "--.-": "q",
-        ".-.": "r",
-        "...": "s",
-        "-": "t",
-        "..-": "u",
-        "...-": "v",
-        ".--": "w",
-        "-..-": "x",
-        "-.--": "y",
-        "--..": "z",
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
     }
+
     # 공백을 기준으로 문자열을 나누기
     letter = letter.split()
     answer = ""
@@ -90,8 +70,7 @@ def solution(letter):
                 answer += value  # 문자열에 알파벳 추가
     return answer
 
-
-# 내 좌우명
+#내 좌우명
 # print(solution('.-.. --- ...- . -- -.-- ... . .-.. ..-.'))
 
 
@@ -114,12 +93,12 @@ def solution(age):
     # 자릿수 별로 나눠 list 형식으로 저장  ex)[1,2,3]
     age = list(map(int, str(age)))
     # 아스키코드 10진수 : a ~ j -> 97 ~ 106
-    answer = ""
+    
+    answer = "" # 변환한 행성의 나이를 저장할 변수
     for i in age:
         # 변환할 숫자에 아스키 코드 값을 더해서 다시 문자로 변환
         answer += chr(i + 97)
     return answer
-
 
 # print(solution(100))
 
@@ -139,28 +118,23 @@ def solution(age):
 
 import math
 
-
 def solution(r1, r2):
     answer = 0
-
     # 바깥쪽 원의 x좌표를 나타내는 i
     for i in range(1, r2 + 1):
         # 원의 방정식을 이용해 y값을 찾음
-        y_r2 = math.sqrt(r2**2 - i**2)
-        # y_r1 = (r2**2 - i**2)**(0.5)
+        y_r2 = math.sqrt(r2**2 - i**2) # y_r2 = (r2**2 - i**2)**(0.5)
 
-        if i > r1:
-            y_r1 = 0
+        if i <= r1: # r1보다 큰 범위만 계산
+             # 원의 방정식을 이용해 y값을 찾음
+            y_r1 = math.sqrt(r1**2 - i**2) # y_r1 = (r1**2 - i**2)**(0.5)
         else:
-            # 원의 방정식을 이용해 y값을 찾음
-            y_r1 = math.sqrt(r1**2 - i**2)
-            # x_r2 = (r1**2 - i**2)**(0.5)
-        answer += math.floor(y_r2) - math.ceil(y_r1) + 1
-        # floor : 내림, ceil : 올림
+            y_r1 = 0
+        
+        answer += math.floor(y_r2) - math.ceil(y_r1) + 1 # floor : 내림, ceil : 올림
     return answer * 4
 
-
-# print(solution(2,3))
+#print(solution(2,3))
 
 #
 # Q.5 10점
@@ -186,12 +160,19 @@ def solution(r1, r2):
 
 def solution(numbers):
     # 정수형 numbers를 문자열로 바꾸고 list형태로 저장
-    list_num = list(map(str, numbers))
+    list_num = list(map(str,numbers)) 
+    # key 를 통하여 정렬할 기준을 정할 수 있다.
+    # reverse 가 True이면 내림차순
+    # 익명함수를 지칭한다. 함수 선언
+    list_num.sort(key = lambda x : x*3,reverse = True) # x를 3번 반복한 문자열을 기준으로 정렬
+    
+    answer = ''
+    for i in list_num:     
+        answer += i
+    
+    # return answer으로 바로 반환 했을 떄 numbers = [0,0]일 때 가장 큰수는 00을 반환한다.(오류)    
+    # return answer
+    return str(int(answer))
 
-    list_num.sort(key=lambda x: x * 3, reverse=True)
-
-    answer = str(int("".join(list_num)))
-    return answer
-
-
-print(solution([3, 30, 34, 5, 9]))
+#print(solution([0,0]))
+#print(solution([3, 30, 34, 5, 9]))
